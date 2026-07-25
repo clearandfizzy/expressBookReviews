3017bc7cc3aaae87ee09b144d7df225ed5cd09b2
Gareth Price <gareth@clearandfizzy.com>
2026-07-25 22:41:16 +0100

    Enhance user authentication and review management functionality

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,53 +7,77 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username) => {
-  const filtered = users.filter(item => item.username === username);
-  return (filtered.length < 1);
+	const filtered = users.filter(item => item.username === username);
+	return (filtered.length < 1);
 }
 
 const authenticatedUser = (username,password) => {
-  const filtered = users.filter(item => (item.username === username && item.password === password));
-  return filtered.length === 1;
+	const filtered = users.filter(item => (item.username === username && item.password === password));
+	return filtered.length === 1;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  const {username, password} = req.body;
+	const {username, password} = req.body;
 
-  if (!authenticatedUser(username, password)) {
-    res.send('Invalid Credentials');
-  }
+	if (!authenticatedUser(username, password)) {
+		return res.send('Invalid Credentials');
+	}
 
-  const token = jwt.sign({
-    data: 'access'
-  }, 'secret', { expiresIn: 60 * 60 });
+	let accessToken = jwt.sign({
+		data: username
+	}, 'access', { expiresIn: 60 * 60 });
 
-  res.send('User signed in');
+	req.session.authorization = {
+		accessToken, username
+	};
+	return res.status(200).send("User successfully logged in");
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  const {isbn} = req.params;
-  const {review} = req.body;
+	const {isbn} = req.params;
+	const {review} = req.body;
+	const username = req.user.data;
 
-  const items = Object.entries(books);
-  const filtered = items.filter(item => item[0] === isbn);
+	const items = Object.entries(books);
+	const filtered = items.filter(item => item[0] === isbn);
 
-  if (filtered.length < 1) {
-    res.send('ISBN not found');
-  }
+	if (filtered.length < 1) {
+		res.send('ISBN not found');
+	}
 
-  console.log(filtered[0][1]);
+	let reviews = Object.entries(books[isbn].reviews);
+	let filtered_reviews = reviews.filter(item => item[1].username === username);
 
-  const book  = filtered[0][1];
-  let reviews = book.reviews.filter(item => item.username !== username);
-  reviews.push({username: '', review});
-  books[isbn].reviews = reviews;
+	if (filtered_reviews.length  === 0) {
+		const reviewLength = books[isbn].reviews.length ?? 0;
+		books[isbn].reviews[reviewLength] =  {username, review};
+		return res.send('Review Added');
+	}
 
-  console.log(books);
+	const key = filtered_reviews[0][0];
+	books[isbn].reviews[key] = {username, review};
+	return res.send('Review Updated');
 
-  res.send('Review Added');
+});
 
+// Add a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+	const {isbn} = req.params;
+	const username = req.user.data;
+
+	const entry = Object.entries(books[isbn].reviews).find(
+		([, item]) => item.username === username
+	);
+
+	if (entry.length > 0) {
+		const [key] = entry;
+		delete books[isbn].reviews[key];
+		return res.send('Book review deleted');
+	}
+
+	return res.send('No review to Delete');
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,23 +1,29 @@
 const express = require('express');
+const axios = require('axios').default;
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
 	const {username, password} = req.body;
 
 	if (username === '' || password === '' || !isValid(username)){
-		res.send('Username is invalid');
+		return res.send('Username is invalid');
 	}
 
 	users.push({username, password});
+	return res.send('User Registered');
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
 	return res.send(JSON.stringify(books));
+});
+
+public_users.get('/task10', async function (req, res) {
+	const result = await axios.get('https://gareth12345-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/');
+	return res.send(result.data);
 });
 
 // Get book details based on ISBN
@@ -28,6 +34,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
 	return res.send(filtered);
 });
 
+public_users.get('/task11', async function (req, res) {
+	const result = await axios.get('https://gareth12345-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/3');
+	return res.send(result.data);
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
 	const {author} = req.params;
@@ -36,12 +47,23 @@ public_users.get('/author/:author',function (req, res) {
 	return res.send(filtered);
 });
 
+public_users.get('/task12', async function (req, res) {
+	const result = await axios.get('https://gareth12345-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/Unknown');
+	return res.send(result.data);
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
 	const {title} = req.params;
 	const items = Object.entries(books);
 	const filtered = items.filter(item => item[1].title === title);
 	return res.send(filtered);
+});
+
+
+public_users.get('/task13', async function (req, res) {
+	const result = await axios.get('https://gareth12345-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/The Epic Of Gilgamesh');
+	return res.send(result.data);
 });
 
 //  Get book review
